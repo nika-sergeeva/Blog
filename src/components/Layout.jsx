3c66c9278aedf9ff2a71639afd61922cfaff145a
Veronika <72826720+marvelousveronika@users.noreply.github.com>
2022-07-09 01:51:44 +0300

    swr

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 
-import Header from './Header/Header'
+import Header from './Header/header'
 import Main from './Main'
 
 const Layout = () => {
